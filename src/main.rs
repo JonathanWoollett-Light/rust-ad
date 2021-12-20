e4cb98ab55cd6d8364d7fb63d9af5e64b653e270
Jonathan <jonathanwoollettlight@gmail.com>
2021-12-20 18:36:53 +0000

    s

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ extern crate rust_ad;
 
 // File for testing.
 
-// #[rust_ad::reverse_autodiff]
-// fn forward(x: f64, y: f32) -> f64 {
-//     let a = 7. * x;
-//     let b = 3. * x;
-//     let c = x + b;
-//     let d = y + b + c;
-//     return d;
-// }
+#[rust_ad::reverse_autodiff]
+fn forward(x: f64, y: f64) -> f64 {
+    let a = 7. * x;
+    let b = 3. * x;
+    let c = x + b;
+    let d = y + b + c;
+    return d;
+}
 
 fn main() {
+    
 }
