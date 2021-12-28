f905ba5ad7a2c61ed6c0c4038cb41990dbef9502
Jonathan <jonathanwoollettlight@gmail.com>
2021-12-28 12:58:15 +0000

    fixes

--- core/src/dict.rs
+++ core/src/dict.rs
@@ -180,39 +180,39 @@ lazy_static::lazy_static! {
         // f32
         // ----------------------------
         // Exponents
-        map.insert(("powi","f64",&["i32"]).into(),ProcedureOutputs::new("f64",forward_powi::<{Type::F64}>,reverse_powi::<{Type::F64}>));
-        map.insert(("powf","f64",&["f64"]).into(),ProcedureOutputs::new("f64",forward_powf::<{Type::F64}>,reverse_powf::<{Type::F64}>));
-        map.insert(("sqrt","f64",&[]).into(),ProcedureOutputs::new("f64",forward_sqrt::<{Type::F64}>,reverse_sqrt::<{Type::F64}>));
-        map.insert(("cbrt","f64",&[]).into(),ProcedureOutputs::new("f64",forward_cbrt::<{Type::F64}>,reverse_cbrt::<{Type::F64}>));
-        map.insert(("exp","f64",&[]).into(),ProcedureOutputs::new("f64",forward_exp::<{Type::F64}>,reverse_exp::<{Type::F64}>));
-        map.insert(("exp2","f64",&[]).into(),ProcedureOutputs::new("f64",forward_exp2::<{Type::F64}>,reverse_exp2::<{Type::F64}>));
-        map.insert(("exp_m1","f64",&[]).into(),ProcedureOutputs::new("f64",forward_exp_m1::<{Type::F64}>,reverse_exp_m1::<{Type::F64}>));
+        map.insert(("powi","f32",&["i32"]).into(),ProcedureOutputs::new("f32",forward_powi::<{Type::F32}>,reverse_powi::<{Type::F32}>));
+        map.insert(("powf","f32",&["f32"]).into(),ProcedureOutputs::new("f32",forward_powf::<{Type::F32}>,reverse_powf::<{Type::F32}>));
+        map.insert(("sqrt","f32",&[]).into(),ProcedureOutputs::new("f32",forward_sqrt::<{Type::F32}>,reverse_sqrt::<{Type::F32}>));
+        map.insert(("cbrt","f32",&[]).into(),ProcedureOutputs::new("f32",forward_cbrt::<{Type::F32}>,reverse_cbrt::<{Type::F32}>));
+        map.insert(("exp","f32",&[]).into(),ProcedureOutputs::new("f32",forward_exp::<{Type::F32}>,reverse_exp::<{Type::F32}>));
+        map.insert(("exp2","f32",&[]).into(),ProcedureOutputs::new("f32",forward_exp2::<{Type::F32}>,reverse_exp2::<{Type::F32}>));
+        map.insert(("exp_m1","f32",&[]).into(),ProcedureOutputs::new("f32",forward_exp_m1::<{Type::F32}>,reverse_exp_m1::<{Type::F32}>));
         // Logs
-        map.insert(("ln","f64",&[]).into(),ProcedureOutputs::new("f64",forward_ln::<{Type::F64}>,reverse_ln::<{Type::F64}>));
-        map.insert(("ln","f64",&[]).into(),ProcedureOutputs::new("f64",forward_ln_1p::<{Type::F64}>,reverse_ln_1p::<{Type::F64}>));
-        map.insert(("log","f64",&["f64"]).into(),ProcedureOutputs::new("f64",forward_log::<{Type::F64}>,reverse_log::<{Type::F64}>));
-        map.insert(("log10","f64",&[]).into(),ProcedureOutputs::new("f64",forward_log10::<{Type::F64}>,reverse_log10::<{Type::F64}>));
-        map.insert(("log2","f64",&[]).into(),ProcedureOutputs::new("f64",forward_log2::<{Type::F64}>,reverse_log2::<{Type::F64}>));
+        map.insert(("ln","f32",&[]).into(),ProcedureOutputs::new("f32",forward_ln::<{Type::F32}>,reverse_ln::<{Type::F32}>));
+        map.insert(("ln_1p","f32",&[]).into(),ProcedureOutputs::new("f32",forward_ln_1p::<{Type::F32}>,reverse_ln_1p::<{Type::F32}>));
+        map.insert(("log","f32",&["f32"]).into(),ProcedureOutputs::new("f32",forward_log::<{Type::F32}>,reverse_log::<{Type::F32}>));
+        map.insert(("log10","f32",&[]).into(),ProcedureOutputs::new("f32",forward_log10::<{Type::F32}>,reverse_log10::<{Type::F32}>));
+        map.insert(("log2","f32",&[]).into(),ProcedureOutputs::new("f32",forward_log2::<{Type::F32}>,reverse_log2::<{Type::F32}>));
         // Trig
-        map.insert(("acos","f64",&[]).into(),ProcedureOutputs::new("f64",forward_acos::<{Type::F64}>,reverse_acos::<{Type::F64}>));
-        map.insert(("acosh","f64",&[]).into(),ProcedureOutputs::new("f64",forward_acosh::<{Type::F64}>,reverse_acosh::<{Type::F64}>));
-        map.insert(("asin","f64",&[]).into(),ProcedureOutputs::new("f64",forward_asin::<{Type::F64}>,reverse_asin::<{Type::F64}>));
-        map.insert(("asinh","f64",&[]).into(),ProcedureOutputs::new("f64",forward_asinh::<{Type::F64}>,reverse_asinh::<{Type::F64}>));
-        map.insert(("atan","f64",&[]).into(),ProcedureOutputs::new("f64",forward_atan::<{Type::F64}>,reverse_atan::<{Type::F64}>));
-        map.insert(("sin","f64",&[]).into(),ProcedureOutputs::new("f64",forward_sin::<{Type::F64}>,reverse_sin::<{Type::F64}>));
-        map.insert(("atanh","f64",&[]).into(),ProcedureOutputs::new("f64",forward_atanh::<{Type::F64}>,reverse_atanh::<{Type::F64}>));
-        map.insert(("cos","f64",&[]).into(),ProcedureOutputs::new("f64",forward_cos::<{Type::F64}>,reverse_cos::<{Type::F64}>));
-        map.insert(("cosh","f64",&[]).into(),ProcedureOutputs::new("f64",forward_cosh::<{Type::F64}>,reverse_cosh::<{Type::F64}>));
-        map.insert(("sinh","f64",&[]).into(),ProcedureOutputs::new("f64",forward_sinh::<{Type::F64}>,reverse_sinh::<{Type::F64}>));
-        map.insert(("tan","f64",&[]).into(),ProcedureOutputs::new("f64",forward_tan::<{Type::F64}>,reverse_tan::<{Type::F64}>));
-        map.insert(("tanh","f64",&[]).into(),ProcedureOutputs::new("f64",forward_tanh::<{Type::F64}>,reverse_tanh::<{Type::F64}>));
+        map.insert(("acos","f32",&[]).into(),ProcedureOutputs::new("f32",forward_acos::<{Type::F32}>,reverse_acos::<{Type::F32}>));
+        map.insert(("acosh","f32",&[]).into(),ProcedureOutputs::new("f32",forward_acosh::<{Type::F32}>,reverse_acosh::<{Type::F32}>));
+        map.insert(("asin","f32",&[]).into(),ProcedureOutputs::new("f32",forward_asin::<{Type::F32}>,reverse_asin::<{Type::F32}>));
+        map.insert(("asinh","f32",&[]).into(),ProcedureOutputs::new("f32",forward_asinh::<{Type::F32}>,reverse_asinh::<{Type::F32}>));
+        map.insert(("atan","f32",&[]).into(),ProcedureOutputs::new("f32",forward_atan::<{Type::F32}>,reverse_atan::<{Type::F32}>));
+        map.insert(("sin","f32",&[]).into(),ProcedureOutputs::new("f32",forward_sin::<{Type::F32}>,reverse_sin::<{Type::F32}>));
+        map.insert(("atanh","f32",&[]).into(),ProcedureOutputs::new("f32",forward_atanh::<{Type::F32}>,reverse_atanh::<{Type::F32}>));
+        map.insert(("cos","f32",&[]).into(),ProcedureOutputs::new("f32",forward_cos::<{Type::F32}>,reverse_cos::<{Type::F32}>));
+        map.insert(("cosh","f32",&[]).into(),ProcedureOutputs::new("f32",forward_cosh::<{Type::F32}>,reverse_cosh::<{Type::F32}>));
+        map.insert(("sinh","f32",&[]).into(),ProcedureOutputs::new("f32",forward_sinh::<{Type::F32}>,reverse_sinh::<{Type::F32}>));
+        map.insert(("tan","f32",&[]).into(),ProcedureOutputs::new("f32",forward_tan::<{Type::F32}>,reverse_tan::<{Type::F32}>));
+        map.insert(("tanh","f32",&[]).into(),ProcedureOutputs::new("f32",forward_tanh::<{Type::F32}>,reverse_tanh::<{Type::F32}>));
         // Misc
-        map.insert(("abs","f64",&[]).into(),ProcedureOutputs::new("f64",forward_abs::<{Type::F64}>,reverse_abs::<{Type::F64}>));
-        map.insert(("ceil","f64",&[]).into(),ProcedureOutputs::new("f64",forward_ceil::<{Type::F64}>,reverse_ceil::<{Type::F64}>));
-        map.insert(("floor","f64",&[]).into(),ProcedureOutputs::new("f64",forward_floor::<{Type::F64}>,reverse_floor::<{Type::F64}>));
-        map.insert(("fract","f64",&[]).into(),ProcedureOutputs::new("f64",forward_fract::<{Type::F64}>,reverse_fract::<{Type::F64}>));
-        map.insert(("recip","f64",&[]).into(),ProcedureOutputs::new("f64",forward_recip::<{Type::F64}>,reverse_recip::<{Type::F64}>));
-        map.insert(("round","f64",&[]).into(),ProcedureOutputs::new("f64",forward_round::<{Type::F64}>,reverse_round::<{Type::F64}>));
+        map.insert(("abs","f32",&[]).into(),ProcedureOutputs::new("f32",forward_abs::<{Type::F32}>,reverse_abs::<{Type::F32}>));
+        map.insert(("ceil","f32",&[]).into(),ProcedureOutputs::new("f32",forward_ceil::<{Type::F32}>,reverse_ceil::<{Type::F32}>));
+        map.insert(("floor","f32",&[]).into(),ProcedureOutputs::new("f32",forward_floor::<{Type::F32}>,reverse_floor::<{Type::F32}>));
+        map.insert(("fract","f32",&[]).into(),ProcedureOutputs::new("f32",forward_fract::<{Type::F32}>,reverse_fract::<{Type::F32}>));
+        map.insert(("recip","f32",&[]).into(),ProcedureOutputs::new("f32",forward_recip::<{Type::F32}>,reverse_recip::<{Type::F32}>));
+        map.insert(("round","f32",&[]).into(),ProcedureOutputs::new("f32",forward_round::<{Type::F32}>,reverse_round::<{Type::F32}>));
 
         // f64
         // ----------------------------
@@ -226,7 +226,7 @@ lazy_static::lazy_static! {
         map.insert(("exp_m1","f64",&[]).into(),ProcedureOutputs::new("f64",forward_exp_m1::<{Type::F64}>,reverse_exp_m1::<{Type::F64}>));
         // Logs
         map.insert(("ln","f64",&[]).into(),ProcedureOutputs::new("f64",forward_ln::<{Type::F64}>,reverse_ln::<{Type::F64}>));
-        map.insert(("ln","f64",&[]).into(),ProcedureOutputs::new("f64",forward_ln_1p::<{Type::F64}>,reverse_ln_1p::<{Type::F64}>));
+        map.insert(("ln_1p","f64",&[]).into(),ProcedureOutputs::new("f64",forward_ln_1p::<{Type::F64}>,reverse_ln_1p::<{Type::F64}>));
         map.insert(("log","f64",&["f64"]).into(),ProcedureOutputs::new("f64",forward_log::<{Type::F64}>,reverse_log::<{Type::F64}>));
         map.insert(("log10","f64",&[]).into(),ProcedureOutputs::new("f64",forward_log10::<{Type::F64}>,reverse_log10::<{Type::F64}>));
         map.insert(("log2","f64",&[]).into(),ProcedureOutputs::new("f64",forward_log2::<{Type::F64}>,reverse_log2::<{Type::F64}>));
--- src/lib.rs
+++ src/lib.rs
@@ -13,20 +13,20 @@
 //! *`typeof` (e.g. [`decltype`](https://en.cppreference.com/w/cpp/language/decltype)) not being currently implemented in Rust makes support more difficult.
 //!
 //! °Support limited to the basic blas-like operations.
-//! 
+//!
 //! Type/s | Support
 //! --- | ---
 //! Floats: `f32` & `f64` | ✓
-//! Intgers: `u16`, `i16` etc. | 
-//! 
+//! Intgers: `u16`, `i16` etc. |
+//!
 //! All mentioned types have support for basic `+`, `-`, `*` and `/` operations if they are implemented.
-//! 
+//!
 //! Support means almost* all the native operations (operations implemented by the library which defines the type) are supported.
-//! 
+//!
 //! *The 'almost' exception is here since some operations may simply not be worth supporting given extremely limited usage, possible deprecation, etc.
-//! 
+//!
 //! For the specifics of operation support see the [rust-ad-core docs](https://docs.rs/rust-ad-core/).
-//! 
+//!
 //! ### Notes on internals
 //! With `cargo expand` when an `.unwrap()` panics it doesn't give a line reference. Using unique panic messages makes locating errors easier. These should help debuging quite specifically (ctrl+f).
 //!
